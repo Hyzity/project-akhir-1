99578bae38aa349f97271e177ef3a2581ed6079a
Anisetus  Bambang Manalu <145880996+Anisetus@users.noreply.github.com>
2024-06-04 15:38:28 +0700

    perbaikan guru

--- resources/views/guru/index.blade.php
+++ resources/views/guru/index.blade.php
@@ -7,8 +7,8 @@
     <div class="container" style="padding-top: 100px; background-color:#F8F4EC;">
         <div class="row">
             <div class="col-12">
-                <div class="container-fluid " style="padding-bottom: 50px;">
-                    <h1 style="text-align: center; padding: 25px 0 25px 0;box-shadow: #33333323 3px 5px 2px;border: #33333345 1px solid; background-color: white;">Data Guru</h1>
+                <div class="container-fluid" style="padding-bottom: 50px;">
+                    <h1 style="text-align: center; padding: 25px 0; box-shadow: #33333323 3px 5px 2px; border: #33333345 1px solid; background-color: white;">Data Guru</h1>
                 </div>
                 <div class="row">
                 </div>
@@ -16,7 +16,7 @@
 
             <div class="row">
                 @forelse ($guru as $g)
-                <div class="col-12 col-md-4 col-lg-3 mb-4 d-flex align-items-stretch" style="padding: 0 20px 0 20px;">
+                <div class="col-12 col-md-6 col-lg-3 mb-4 d-flex align-items-stretch">
                     <div class="card w-100 hover-effect" style="transition: transform 0.3s, box-shadow 0.3s; border: 1px solid #ddd;">
                         <a href="{{ route('guru.show', $g->id)}}">
                             <div class="card-header p-0 position-relative">
@@ -29,7 +29,7 @@
                         </a>
                         <div class="card-body">
                             <h5 class="card-title" style="text-align: center;">{{ $g->nama }}</h5>
-                            <p class="card-text"><s trong>Bidang Keahlian:</strong> {{ $g->bidang_keahlian }}</p>
+                            <p class="card-text"><strong>Bidang Keahlian:</strong> {{ $g->bidang_keahlian }}</p>
                             <p class="card-text"><strong>Pendidikan:</strong> {{ $g->pendidikan }}</p>
                             <p class="card-text"><strong>Tempat Lahir:</strong> {{ $g->tempat_lahir }}</p>
                             <p class="card-text"><strong>Tanggal Lahir:</strong> {{ $g->tgl_lahir }}</p>
