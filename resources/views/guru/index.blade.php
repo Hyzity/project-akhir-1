dcfe48b134d0e1c447a935af938975a222532bb9
Hyzity <134353555+Hyzity@users.noreply.github.com>
2024-06-09 16:20:05 +0700

    Final Decision
    End

--- resources/views/guru/index.blade.php
+++ resources/views/guru/index.blade.php
@@ -8,8 +8,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="container-fluid" style="padding-bottom: 50px;">
-                        <h1
-                            style="text-align: center; padding: 25px 0; box-shadow: #33333323 3px 5px 2px; border: #33333345 1px solid; background-color: white;">
+                        <h1 style="text-align: center; padding: 25px 0; box-shadow: #33333323 3px 5px 2px; border: #33333345 1px solid; background-color: white;">
                             Data Guru</h1>
                     </div>
                 </div>
@@ -17,25 +16,23 @@
 
             <div class="row container">
                 @forelse ($guru as $g)
-                    <div class="col-12 col-md-6 col-lg-3 mb-4 d-flex align-items-stretch">
-                        <div class="card w-100 hover-effect"
-                            style="transition: transform 0.3s, box-shadow 0.3s; border: 1px solid #ddd;">
-                            <a href="{{ route('guru.show', $g->id) }}">
-                                <div class="card-img-container">
-                                    @if ($g->foto)
-                                        <img src="{{ asset('uploads/img/guru/' . $g->foto) }}" class="card-img-top"
-                                            alt="Guru Thumbnail">
-                                    @else
-                                        <img src="{{ asset('img/guru/default.jpg') }}" class="card-img-top"
-                                            alt="Guru Thumbnail">
-                                    @endif
-                                </div>
-                                <div class="card-body">
-                                    <h6 class="card-title" style="text-align: center;">{{ $g->nama }}</h6>
-                                </div>
-                            </a>
-                        </div>
+                <div class="col-12 col-md-6 col-lg-3 mb-4 d-flex align-items-stretch">
+                    <div class="card w-100 hover-effect" style="transition: transform 0.3s, box-shadow 0.3s; border: 1px solid #ddd; padding: 20px; margin: 10px;">
+                        <a href="{{ route('guru.show', $g->id) }}">
+                            <div class="card-img-container" style="position: relative; width: 100%; padding-top: 75%; /* 4:3 aspect ratio */ overflow: hidden;">
+                                @if ($g->foto)
+                                    <img src="{{ asset('uploads/img/guru/' . $g->foto) }}" class="card-img-top" alt="Guru Thumbnail" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
+                                @else
+                                    <img src="{{ asset('img/guru/default.jpg') }}" class="card-img-top" alt="Guru Thumbnail" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
+                                @endif
+                            </div>
+                            <div class="card-body">
+                                <h6 class="card-title" style="text-align: center;">{{ $g->nama }}</h6>
+                            </div>
+                        </a>
                     </div>
+                </div>
+                
 
                 @empty
                     <div class="col-12">
--- resources/views/guru/show.blade.php
+++ resources/views/guru/show.blade.php
@@ -10,37 +10,24 @@
         <h3 style="padding-top: 150px;">{{ $guru->nama }}</h3>
     </div>
 
-    <div class="blog-details-content" style="padding: 50px;">
+    <div class="blog-details-content">
         <div class="container">
-            <div
-                style="position: relative; width: 1131px; height: 505.4px; filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25)); background-color: white; border-radius: 20px;">
-                <div
-                    style="display: flex; height: 100%; align-items: center; background-color: #860000; border-radius: 20px;">
-                    <div style="display: flex; justify-content: center; align-items: center; padding: 5px;">
+            <div class="card">
+                <div class="card-content">
+                    <div class="image-container">
                         @if ($guru->foto)
-                            <div class="image-container">
-                                <img src="{{ asset('uploads/img/guru/' . $guru->foto) }}" alt="Foto Guru"
-                                    class="square-image">
-                            </div>
+                            <img src="{{ asset('uploads/img/guru/' . $guru->foto) }}" alt="Foto Guru" class="square-image">
                         @else
-                            <div class="image-container">
-                                <img src="{{ asset('img/guru/default.jpg') }}" alt="Foto Guru" class="square-image">
-                            </div>
+                            <img src="{{ asset('img/guru/default.jpg') }}" alt="Foto Guru" class="square-image">
                         @endif
                     </div>
-
-                    <div style="margin-left: 100px;">
-                        <ul style="list-style-type: none; padding: 0;">
-                            <li style="margin-bottom: 30px; color: white; font-size: 25px;"><strong>Nama: </strong>
-                                {{ $guru->nama }}</li>
-                            <li style="margin-bottom: 30px; color: white; font-size: 25px;"><strong>Bidang
-                                    Keahlian:</strong> {{ $guru->bidang_keahlian }}</li>
-                            <li style="margin-bottom: 30px; color: white; font-size: 25px;"><strong>Pendidikan:</strong>
-                                {{ $guru->pendidikan }}</li>
-                            <li style="margin-bottom: 30px; color: white; font-size: 25px;"><strong>Tempat Lahir:</strong>
-                                {{ $guru->tempat_lahir }}</li>
-                            <li style="margin-bottom: 30px; color: white; font-size: 25px;"><strong>Tanggal Lahir:</strong>
-                                {{ $guru->tgl_lahir }} </li>
+                    <div class="details">
+                        <ul>
+                            <li><strong>Nama: </strong>{{ $guru->nama }}</li>
+                            <li><strong>Bidang Keahlian:</strong> {{ $guru->bidang_keahlian }}</li>
+                            <li><strong>Pendidikan:</strong> {{ $guru->pendidikan }}</li>
+                            <li><strong>Tempat Lahir:</strong> {{ $guru->tempat_lahir }}</li>
+                            <li><strong>Tanggal Lahir:</strong> {{ $guru->tgl_lahir }}</li>
                         </ul>
                     </div>
                 </div>
@@ -48,12 +35,10 @@
         </div>
     </div>
     <style>
-        .image-container {
+        /* .image-container {
             position: relative;
             width: 485px;
-            /* Desired width */
             padding-bottom: 100%;
-            /* 1:1 aspect ratio */
             overflow: hidden;
             border-radius: 20px;
             border: 4px solid #860000;
@@ -67,7 +52,86 @@
             width: 100%;
             height: 100%;
             object-fit: cover;
-            /* Maintain aspect ratio, crop if necessary */
+        } */
+    </style>
+    <style>
+        .blog-details-content {
+            padding: 50px;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+        }
+
+        .card {
+            position: relative;
+            width: 100%;
+            max-width: 1131px;
+            background-color: white;
+            border-radius: 20px;
+            filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        }
+
+        .card-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 20px;
+            background-color: #860000;
+            border-radius: 20px;
+            color: white;
+        }
+
+        .image-container {
+            width: 300px; /* Fixed width for cropping */
+            height: 300px; /* Fixed height for cropping */
+            overflow: hidden;
+            border-radius: 20px;
+        }
+
+        .square-image {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .details {
+            width: 100%;
+        }
+
+        .details ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        .details li {
+            margin-bottom: 20px;
+            font-size: 20px;
+        }
+
+        @media (min-width: 768px) {
+            .card-content {
+                flex-direction: row;
+                align-items: flex-start;
+                padding: 40px;
+            }
+
+            .image-container {
+                margin-right: 50px;
+                margin-bottom: 0;
+                width: 300px; /* Fixed width for cropping */
+                height: 300px; /* Fixed height for cropping */
+            }
+
+            .details {
+                margin-left: 100px;
+            }
+
+            .details li {
+                font-size: 25px;
+                margin-bottom: 30px;
+            }
         }
     </style>
 @stop
